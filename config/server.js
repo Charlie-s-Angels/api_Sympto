30e60f54fabb64422bc0e1df58430b3b7af63144
jrkinouani <kinouani@gmail.com>
2020-12-17 11:55:32 +0400

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed10152ea9ca01ff60f2730f1aca68f0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ddc630b-077e-4801-8f53-3bc9133d3482'
+};
